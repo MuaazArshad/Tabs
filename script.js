86d756a6cee6869eeb42715e80aa321fb350a356
MuaazArshad <mooeez.arshad@gmail.com>
2024-01-17 16:00:14 +0500

    Added Functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const btns = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.content');
+
+btns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		btns.forEach((btn) => {
+			btn.classList.remove('active');
+		});
+		contents.forEach((content) => {
+			content.classList.remove('active');
+		});
+		e.currentTarget.classList.add('active');
+		let id = e.currentTarget.innerHTML;
+		const con = document.getElementById(id);
+		con.classList.add('active');
+	});
+});
